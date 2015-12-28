1114023d78c144c443ad324a04131250f20f9117
545577173@qq.com <545577173@qq.com>
2015-12-28 14:34:17 +0800

    pom  fix

--- src/main/java/com/controller/LoginController.java
+++ src/main/java/com/controller/LoginController.java
@@ -5,8 +5,11 @@
 
 package com.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
 
 /**
  * @Description: LoginController
@@ -14,11 +17,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @CreateTime: 2015-12-27 21:38
  */
 @Controller
+@RequestMapping(value = "/login")
 public class LoginController {
 
-    @RequestMapping(value = "index")
+    Logger logger = org.apache.log4j.Logger.getLogger(LoginController.class);
+    @RequestMapping(value = "/index")
     public String welcome(){
-
+        logger.debug("into LoginController................................");
         System.out.print("sdfsfsdfsd");
         return "jsp/index";
     }
